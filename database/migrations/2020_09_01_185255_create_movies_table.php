f885f53844867c6b0b82e53af874b94638739086
ivanamilovac <antesevicivana@gmail.com>
2020-09-01 21:06:00 +0200

    created movies table, and manualy persisted data to movies table

---
+++ database/migrations/2020_09_01_185255_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('genre');
+            $table->string('director');
+            $table->text('story_line');
+            $table->integer('year');
+            $table->timestamps();
+        });
+
+        //Title, žanr (genre), ime režisera (director), godinu kada je snimljen i ceo storyline (zaplet)
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
