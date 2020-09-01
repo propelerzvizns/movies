b9f7e2ac0f37e518f838ad594f4d5f92248f7bde
ivanamilovac <antesevicivana@gmail.com>
2020-09-01 21:42:03 +0200

    imported bootstrap, crated layout file and navigation

--- resources/views/movies/all.blade.php
+++ resources/views/movies/all.blade.php
@@ -1,11 +1,16 @@
+@extends('layouts.app')
+@section('title', 'movies')
+@section('content')
+
+    @foreach ($movies as $movie)
+    <a href="{{route('singleMovie', ['id' => $movie->id])}}">{{$movie->title}}</a><br>
+    {{$movie->genre}}<br>
+    {{$movie->director}}<br>
+    {{$movie->story_line}}<br>
+    {{$movie->year}}<br>
+    @endforeach
+    
+@endsection
 
 
-
-@foreach ($movies as $movie)
-<a href="{{route('singleMovie', ['id' => $movie->id])}}">{{$movie->title}}</a><br>
-{{$movie->genre}}<br>
-{{$movie->director}}<br>
-{{$movie->story_line}}<br>
-{{$movie->year}}<br>
-@endforeach
 
--- resources/views/movies/single.blade.php
+++ resources/views/movies/single.blade.php
@@ -1,5 +1,11 @@
-{{$movie->title}}<br>
-{{$movie->genre}}<br>
-{{$movie->director}}<br>
-{{$movie->story_line}}<br>
-{{$movie->year}}<br>
+@extends('layouts.app')
+@section('title', 'movies')
+@section('content')
+
+    {{$movie->title}}<br>
+    {{$movie->genre}}<br>
+    {{$movie->director}}<br>
+    {{$movie->story_line}}<br>
+    {{$movie->year}}<br>
+
+@endsection
